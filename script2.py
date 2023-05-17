3b1549d942e68e84c0287846ed225b44aa8cac70
bhthakka <133912152+bhthakka@users.noreply.github.com>
2023-05-17 16:20:31 -0400

    Modified both the scripts in repo clone
    -Deleted a print() statement from script1.py
    -Added two print() statement to script2.py

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 print("There should be one-- and preferably only one --obvious way to do it.")
 print("Beautiful s better than ugly.")
 print("Flat is better than nested.")
+print("love to watch sky")
